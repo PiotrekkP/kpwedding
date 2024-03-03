975acaf6f1d67b8bae892655713ed2610929e7a8
Piotr Paluszek <piotr.paluszek@nokia.com>
2024-03-03 23:18:29 +0100

    useeffect remove

--- src/AppearanceConfirmation.js
+++ src/AppearanceConfirmation.js
@@ -1,5 +1,5 @@
 import SiteHeader from './SiteHeader';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 export default function AppearanceConfirmation() {
     const contentContainerStyle = {
