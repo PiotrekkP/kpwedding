53739e6bcd82f4a56fc3c2b911d4c22a483d8691
Piotr Paluszek <piotr.paluszek@nokia.com>
2024-03-03 15:20:42 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Test from './Test'
-import { useNavigate } from 'react-router-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Test from './Test';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
-  const navigate = useNavigate();
-
-  const handleDropdownItemClick = (targetPage) => {
-    navigate(targetPage);
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p onClick={()=> handleDropdownItemClick('/test')}>Test</p>
-      </header>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/test" element={<Test/>}/>
-      </Routes>
-    </BrowserRouter>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Test/>}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Test.js
+++ src/Test.js
@@ -1,7 +1,21 @@
+import logo from './logo.svg';
 function Test() {
     return (
-      <div className="App">
-        <p>test</p>
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
