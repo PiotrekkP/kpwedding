1f808bca66dbd0ef9844744dc2b4e2a0601bce8f
Piotr Paluszek <piotr.paluszek@nokia.com>
2024-03-03 14:20:34 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './Test'
+import React, {useState, useEffect, useRef} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  let menuRef = useRef();
+
+  useEffect(()=> {
+    let handler = (e)=>{
+      if(!menuRef.current.contains(e.target)){
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handler);
+
+    return()=>{
+      document.removeEventListener("mousedown", handler);
+    }
+  });
+
+  const handleDropdownItemClick = (targetPage) => {
+    navigate(targetPage);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +42,13 @@ function App() {
         >
           Learn React
         </a>
+        <p onClick={()=> handleDropdownItemClick('/test')}>Test</p>
       </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/test" element={<Test/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/Test.js
@@ -0,0 +1,9 @@
+function Test() {
+    return (
+      <div className="App">
+        <p>test</p>
+      </div>
+    );
+  }
+  
+  export default Test;
