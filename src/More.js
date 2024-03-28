e49eb96318cfc93a0aec707c76b2cda40c50444b
Piotr Paluszek <piotr.paluszek@nokia.com>
2024-03-28 23:51:46 +0100

    dodanie danych na stronie "wiecej"

--- src/More.js
+++ src/More.js
@@ -10,21 +10,22 @@ useEffect(() => {
         <SiteHeader></SiteHeader>
         <p className="p-site-title">Więcej</p>
         <p className="p-more-left-info">Zespół</p>
-        <p className="p-more-right-details">to 4 you</p>
+        <p className="p-more-right-details">For You</p>
         <p className="p-more-left-info">Fotograf</p>
-        <p className="p-more-right-details">...</p>
+        <p className="p-more-right-details">BestDay Fotografia</p>
         <p className="p-more-left-info">Kamerzysta</p>
-        <p className="p-more-right-details">...</p>
+        <p className="p-more-right-details">VSTwedding</p>
         <p className="p-more-left-info">Dekoracje</p>
-        <p className="p-more-right-details">...</p>
-        <p className="p-more-left-info">Cukiernia</p>
-        <p className="p-more-right-details">...</p>
-        <p className="p-more-left-info">Strona</p>
-        <p className="p-more-right-details">Madera z Palcem</p>
-        <p className="p-more-left-info">Sala</p>
-        <p className="p-more-right-details">...</p>
+        <p className="p-more-right-details">Becolor Pracownia</p>
+        <p className="p-more-left-info">Słodki stół i tort</p>
+        <p className="p-more-right-details">Cafe Słodziak</p>
         <p className="p-more-left-info">Drink Bar</p>
-        <p className="p-more-right-details">...</p>
+        <p className="p-more-right-details">Drink Up!</p>
+        <p className="p-more-left-info">Sala</p>
+        <p className="p-more-right-details">Oranżeria Przyjęcia</p>
+        <p className="p-more-left-info">Strona</p>
+        <p className="p-more-right-details">Dominika Zdaniewicz</p>
+        <p className="p-more-right-details">Piotr Paluszek</p>
         <p className="p-contact-details-kinga-m"><a className='a-tel' href='tel:+48504386599'>Kinga +48 504 386 599</a></p>
         <p className="p-contact-details-patryk-m"><a className='a-tel' href='tel:+48666174152'>Patryk +48 666 174 152</a></p>
     </div>
--- src/Thanks.js
+++ src/Thanks.js
@@ -10,7 +10,7 @@ const Thanks = () => {
     
     <div className="content-container-thanks">
       <SiteHeader></SiteHeader>
-      <p className="p-thanks">Dziekujemy! <br></br>Do zobaczenia 08.06.2024 r.</p>
+      <p className="p-thanks">Dziękujemy! <br></br>Do zobaczenia 08.06.2024 r.</p>
       <p className="p-contact-details-kinga-thanks"><a className='a-tel' href='tel:+48504386599'>Kinga +48 504 386 599</a></p>
       <p className="p-contact-details-patryk-thanks"><a className='a-tel' href='tel:+48666174152'>Patryk +48 666 174 152</a></p>
     </div>
