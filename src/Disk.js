f975395f662af500b4d7a7ff90f856b7292c3703
Piotr Paluszek <piotr.paluszek@nokia.com>
2024-06-08 08:21:05 +0200

    dodaj

--- src/Disk.js
+++ src/Disk.js
@@ -15,13 +15,13 @@ const Disk = () => {
         Zdjęcia
       </p>
       <a href="https://drive.google.com/drive/folders/1nFrfW9W9joB5tQHAC32krngFaMuSsSPz?usp=drive_link" className="p-map-right-links">
-        Wrzuć i przeglądaj zdjęcia
+        Dodaj i przeglądaj zdjęcia
       </a>
       <p className="p-map-left-info">
         Filmy
       </p>
       <a href="https://drive.google.com/drive/folders/1PjNXJG4zO188NbHYklyOM-UeOhBI8lrF?usp=sharing" className="p-map-right-links">
-        Wrzuć i przeglądaj filmy 
+        Dodaj i przeglądaj filmy 
       </a>
       
       <p className="p-contact-details-kinga-d"><a className='a-tel' href='tel:+48504386599'>Kinga +48 504 386 599</a></p>
